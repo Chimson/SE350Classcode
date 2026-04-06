ddd22ff30c113ac5f6ef1e6ccc77c76bbccc522d
Chimson <bensonharkison10@hotmail.com>
2026-04-06 18:55:10 -0500

    added test class

--- wrksp/350class/tests/shapestests/RectangleTest.java
+++ wrksp/350class/tests/shapestests/RectangleTest.java
@@ -1,17 +1,33 @@
 package shapestests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import shapes.Rectangle;
 
 class RectangleTest {
   
+  private Rectangle square; 
+  
+  @BeforeEach
+  void testRectangles() {
+    square = new Rectangle(0, 0, "red", 4, 4);
+  }
+  
   @Test
-  void area_returnsWidthTimesHeight() {
+  void areaReturnsWidthTimesHeight() {
     Rectangle r = new Rectangle(0, 0, "blue",
       4, 5);
-    assertEquals(20, r.area());
+    assertEquals(20, r.area());  // can also add message on fail
   }
+  
+  @Test 
+  void rectIsSquare() {
+    assertTrue(square.isSquare());
+  } 
+  
+  
   
   
 }
